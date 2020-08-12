8dae90a5e7d1001b0bd4792b2db5bc95125fdc6f
jeromepeng183 <69374380+jeromepeng183@users.noreply.github.com>
2020-08-11 23:44:59 -0400

    Add files via upload

---
+++ Month_Abbrev.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+print('\'大家好\'')
+
+
+# In[7]:
+
+
+months=('JanFebMarAprMayJunJulAugSepOctNovDec')
+n=input('请输入月份数(1-12):')
+
+pos=(int(n)-1)*3
+monthAbbrev=months[pos:pos+3]
+print('月份简写是'+monthAbbrev+'.')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
