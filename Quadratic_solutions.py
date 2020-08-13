1050a8f37c35f19413cb9273d5027603fdecae0c
jeromepeng183 <69374380+jeromepeng183@users.noreply.github.com>
2020-08-13 18:54:47 -0400

    Add files via upload

---
+++ Quadratic_solutions.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import math
+
+
+# In[8]:
+
+
+def main():
+    print('This program find the real solutions to a quadratic\n')
+    a,b,c=eval(input('Please enter the coefficients(a,b,c):'))
+    delta=pow(b,2)-4*a*c
+    if delta>=0:
+        discRoot=math.sqrt(delta)
+        root1=(-b+discRoot)/(2*a)
+        root2=(-b-discRoot)/(2*a)
+        print('\nThe solutions are:',root1,root2)
+    else:
+        print('The equation has no real roots')
+main()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
